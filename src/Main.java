b213d58ad2d83793d4038d97095a2fced2b16781
alex <lyblinskii.shx@gmail.com>
2015-07-05 20:48:15 -0400

    Just a error message output format change.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import util.AbstractCmdProgram;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        AbstractCmdProgram cmdProgram = new dna.Main();
+        try {
+            cmdProgram.run(args);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/dna/Main.java
@@ -0,0 +1,58 @@
+package dna;
+
+import util.AbstractCmdProgram;
+
+import java.io.*;
+
+public class Main extends AbstractCmdProgram {
+
+    @Override
+    protected void doWork(String[] args) throws FileNotFoundException {
+        char x;
+        int adenineCounter = 0, cytosineCounter = 0, guanineCounter = 0, thymineCounter = 0;
+        try {
+            while (inputStream.available() > 0) {
+                x = (char)inputStream.readByte();
+                switch (x) {
+                    case 'A':
+                        adenineCounter++;
+                        break;
+
+                    case 'C':
+                        cytosineCounter++;
+                        break;
+
+                    case 'T':
+                        thymineCounter++;
+                        break;
+
+                    case 'G':
+                        guanineCounter++;
+                        break;
+                }
+            }
+            System.out.println();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        outputStream.println(adenineCounter + " " + cytosineCounter + " " +
+        guanineCounter + " " + thymineCounter);
+    }
+
+    @Override
+    protected void initInputFileName() {
+        inputFileName = "src/dna/input.txt";
+    }
+
+    @Override
+    protected void initOutputFileName() {
+        outputFileName = "src/dna/output.txt";
+    }
+
+    @Override
+    protected void initDescription() {
+        description = "Given: A DNA string s of length at most 1000 nt.\n" +
+                "Return: Four integers (separated by spaces) counting the respective number " +
+                "of times that the symbols 'A', 'C', 'G', and 'T' occur in s.";
+    }
+}
