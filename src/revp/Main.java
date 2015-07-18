4d1f174bba99daf4cfb9ebe7493409af1b6bbed9
alex <lyblinskii.shx@gmail.com>
2015-07-18 12:56:33 -0400

    Solved Locating Restriction Sites problem.

--- src/revp/Main.java
+++ src/revp/Main.java
@@ -4,6 +4,7 @@ import util.AbstractCmdProgram;
 import util.FastaReader;
 
 import java.io.FileNotFoundException;
+import java.util.LinkedList;
 
 /**
  * Problem
@@ -16,21 +17,52 @@ import java.io.FileNotFoundException;
  * length between 4 and 12. You may return these pairs in any order.
  */
 public class Main extends AbstractCmdProgram {
+    class ComplementPalindromeLocus {
+        public int position = 0;
+        public int length = 0;
+
+        public ComplementPalindromeLocus(int position, int length) {
+            this.position = position;
+            this.length = length;
+        }
+
+        @Override
+        public String toString() {
+            return Integer.toString(position) + " " + Integer.toString(length);
+        }
+    }
+
+    private LinkedList<ComplementPalindromeLocus> result = null;
+
     @Override
     protected void doWork(String[] args) throws FileNotFoundException {
-        byte[] data = {'G', 'A', 'T', 'C'};
-        System.out.println("isReversePalindrome(\"GCATAC\") " + isReversePalindrome(data, 0, data.length));
-
+        result = new LinkedList<>();
         FastaReader fastaReader = new FastaReader(inputStream);
         for (FastaReader.FastaRecord rec : fastaReader.parse()) {
-            System.out.println(rec);
+            processFastaRecord(rec);
+        }
+
+        for (ComplementPalindromeLocus loc : result) {
+            outputStream.println(loc);
+        }
+    }
+
+    private void processFastaRecord(FastaReader.FastaRecord rec) {
+        byte[] data = rec.data;
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 4; j <= 12; j += 2) {
+                if (isReversePalindrome(data, i, j)) {
+                    result.add(new ComplementPalindromeLocus(i + 1, j));
+                }
+            }
         }
     }
 
     boolean isReversePalindrome(byte[] data, int offset, int length) {
         int last = offset + length;
+        if (last > data.length) return false;
         for (int i = 0; i < length; i++) {
-            System.out.println("cmp " + data[offset + i] + " : " + complement(data[last - 1 - i]));
+//            System.out.println("cmp " + (char)(data[offset + i]) + " : " + (char)(complement(data[last - 1 - i])));
             if (data[offset + i] != complement(data[last - 1 - i])) return false;
         }
         return true;
