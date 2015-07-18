71c83aff28cd56fb17d98153b9eb7f7ce746cc94
alex <lyblinskii.shx@gmail.com>
2015-07-18 12:44:34 -0400

    Added util.FastaReader - reader for files in ASCII FASTA format. Added skeleton of revp problem.

--- src/revc/Main.java
+++ src/revc/Main.java
@@ -44,7 +44,8 @@ public class Main extends AbstractCmdProgram {
     @Override
     protected void initDescription() {
         description = "In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'.\n" +
-                "The reverse complement of a DNA string s is the string sc formed by reversing the symbols of s, then taking the complement of each symbol (e.g., the reverse complement of \"GTCA\" is \"TGAC\").\n" +
+                "The reverse complement of a DNA string s is the string sc formed by reversing the symbols of s,\n" +
+                "then taking the complement of each symbol (e.g., the reverse complement of \"GTCA\" is \"TGAC\").\n" +
                 "Given: A DNA string s of length at most 1000 bp.\n" +
                 "Return: The reverse complement sc of s.\n";
     }
