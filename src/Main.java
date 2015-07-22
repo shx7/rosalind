b01a6b3ff5d669d0175470ac99a2f1d4b3053aae
alex <lyblinskii.shx@gmail.com>
2015-07-21 20:19:52 -0400

    Solved Overlap Graphs.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        AbstractCmdProgram cmdProgram = new tran.Main();
+        AbstractCmdProgram cmdProgram = new grph.Main();
         try {
             cmdProgram.run(args);
         } catch (IOException e) {
